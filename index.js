99a93007e2ba4ab64609c2a7e72cedf8769e98ae
ecdavis15 <davisethan93@gmail.com>
2016-06-23 01:05:38 -0700

    MongoDB added to store pokemon stats

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var path = require('path')
 
 var app = express()
+var db = mongojs('mongodb://127.0.0.1:27017/stats', ['pokemon'])
 
-app.set('views', './views')
+app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', function(req, res) {
-  res.render('index')
+  db.pokemon.findOne({}, function(err, docs) {
+    var pokemon = docs
+    res.render('index', {
+      pokemon: JSON.stringify(pokemon)
+    })
+  })
 })
 
 app.listen('3000')
